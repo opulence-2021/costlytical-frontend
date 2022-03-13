633480f26439b47fc3d2ede669bac4645c81a977
saran-pathmanathan <saravanagobi.20200692@iit.ac.lk>
2022-03-13 11:46:00 +0530

    add json

--- src/pages/pending/PendingConfirmation.jsx
+++ src/pages/pending/PendingConfirmation.jsx
@@ -5,7 +5,8 @@ import './PendingConfirmation.css'
 import NewSideBar from "../newPage/NewSideBar";
 import Table from '../../components/Table'
 const PendingConfirmation = () => {
-  const list = [{ estimateNo: "CE-003", date: "01/02/2022", projectName: "StationarySet", noOfModel: "05", totalCost: "2,500" }]
+  const list = [{ estimateNo: "CE-003", date: "01/02/2022", projectName: "Stationary Set", noOfModel: "05", totalCost: "2,500" },
+  { estimateNo: "CE-002", date: "02/03/2022", projectName: "Infusion Pump", noOfModel: "06", totalCost: "5,500" }]
   return (
     <div className="container">
       <div className="nav">
