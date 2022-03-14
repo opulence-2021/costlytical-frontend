5b225324318b6e43bd22350caf64e7255404efe0
saran-pathmanathan <saravanagobi.20200692@iit.ac.lk>
2022-03-14 09:11:15 +0530

    alert fix

--- src/pages/pending/PendingDetail.jsx
+++ src/pages/pending/PendingDetail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 //styles
 import './PendingDetail.css';
 //Components
@@ -8,12 +8,30 @@ import NewSideBar from "../newPage/NewSideBar";
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
-
+import IconButton from '@mui/material/IconButton';
+import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 const PendingDetail = () => {
+    const [open, setOpen] = useState(false);
+    const [success, setSuccess] = useState(false);
+
     const list = [{ modelName: "Base v2", material: "PLA+", layerHeight: "0.28cm", infill: "20", subTotal: "1000", quantity: "01", lineTotal: "1000", score: 75 },
     { modelName: "Base v2", material: "PLA+", layerHeight: "0.28cm", infill: "20", subTotal: "250", quantity: "02", lineTotal: "500", score: 75 }]
+
+    const handleConfirm = (alert) => {
+        setOpen(true);
+        if (alert === 'success') {
+            setSuccess(true)
+        }
+    }
+    const handleClose = () => {
+        setOpen(false);
+        setSuccess(false);
+    }
 
     return (
         <div className="container">
@@ -41,7 +59,7 @@ const PendingDetail = () => {
                         <TableDetail list={list} />
                     </div>
                     <div className="container__decline">
-                        <button className='button__decline'>Decline Offer</button>
+                        <button onClick={() => handleConfirm("error")} className='button__decline'>Decline Offer</button>
                     </div>
                     <div className="container__delivery">
                         <div className="box__store">
@@ -59,13 +77,42 @@ const PendingDetail = () => {
                             <TextField sx={{ marginRight: 4 }} id="country" label="Country" variant="standard" />
                         </div>
                         <div className="container__confirm">
-                            <button className='btn__confirm'>Confirm Order</button>
+                            <button onClick={() => handleConfirm("success")} className='btn__confirm'>Confirm Order</button>
                         </div>
                     </div>
-
                 </div>
             </div>
-        </div>
+            {open && <Box sx={{
+                backgroundColor: "rgba(0, 0, 0, 0.486)", width: '100%', height: "100vh", position: "fixed", top: "0", left: "0"
+            }} component='div'>
+                <Collapse sx={{
+                    width: '50%',
+                    height: "25%",
+                    position: "absolute",
+                    top: '25%',
+                    left: '25%',
+                }} in={open}>
+                    < Alert
+                        severity={success ? 'success' : "error"}
+                        action={
+                            < IconButton
+                                aria-label="close"
+                                color="inherit"
+                                size="small"
+                                onClick={() => {
+                                    handleClose()
+                                }}
+                            >
+                                <CloseIcon fontSize="inherit" />
+                            </IconButton>
+                        }
+                        sx={{ mb: 0, padding: "2rem" }}
+                    >
+                        Order{success ? ' Request Submitted ' : ' Declined '}Successfully
+                    </Alert>
+                </Collapse >
+            </ Box>}
+        </div >
     )
 }
 export default PendingDetail;
