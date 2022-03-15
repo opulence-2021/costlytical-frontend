535d34965bb454418298288e115dd41efb7c7c7c
saran-pathmanathan <saravanagobi.20200692@iit.ac.lk>
2022-03-15 10:43:51 +0530

    navbar color fix

--- src/pages/pending/SideBarPending.jsx
+++ src/pages/pending/SideBarPending.jsx
@@ -22,32 +22,32 @@ const SideBarPending = () => {
                 <Link to="/home">
                     <div className="navItem" id="navHome_1">
                         <img src={home} class="navImage" alt="home" />
-                        <h2 className="navHeading">Home</h2>
+                        <h2 className="navHeading_3">Home</h2>
                     </div>
                 </Link>
                 <Link to="/new">
                     <div className="navItem" id="">
                         <img src={newI_o} class="navImage svg" alt="new" />
-                        <h2 className="navHeading">Create New Project</h2>
+                        <h2 className="navHeading_3">Create New Project</h2>
                     </div>
                 </Link>
                 <Link to="/pending">
                     <div className="navItem" id="navPending_1">
                         <div id="selectBlock"></div>
                         <img src={pending} class="choosen navImage" alt="pending" />
-                        <h2 className="navHeading">Pending Confirmation</h2>
+                        <h2 className="navHeading_3 selected">Pending Confirmation</h2>
                     </div>
                 </Link>
                 <Link to="/awaiting">
                     <div className="navItem" id="navAwaiting_1">
                         <img src={awaiting} class="navImage" alt="Awaiting" />
-                        <h2 className="navHeading">Awaiting Projects</h2>
+                        <h2 className="navHeading_3">Awaiting Projects</h2>
                     </div>
                 </Link>
                 <Link to="/previous">
                     <div className="navItem" id="navPrevious_1">
                         <img src={previous} class="navImage" alt="Previous" />
-                        <h2 className="navHeading">Previous Projects</h2>
+                        <h2 className="navHeading_3">Previous Projects</h2>
                     </div>
                 </Link>
             </div>
