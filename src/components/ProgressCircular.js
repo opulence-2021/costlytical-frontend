156d125834c4447d1aba123053ccfb4d8c65b242
saran-pathmanathan <95270988+saran-pathmanathan@users.noreply.github.com>
2022-03-17 21:27:49 +0530

    Create Pending Confirmation Page (#29)
    * add pending folder
    
    * make table component
    
    * create tableDetail
    
    * create tableDetail and sty;e
    
    * finish pendingDetail page
    
    * remove infill

---
+++ src/components/ProgressCircular.js
@@ -0,0 +1,103 @@
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import Popover from "@mui/material/Popover";
+
+const ProgressCircular = ({ progress }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setTimeout(() => {
+      setAnchorEl(null);
+    }, 2000);
+  };
+  const handleClick = () => {
+    alert("Hii");
+  };
+  const open = Boolean(anchorEl);
+  return (
+    <>
+      <Box sx={{ position: "relative", display: "inline-flex" }}>
+        <CircularProgress
+          color="success"
+          variant="determinate"
+          value={progress}
+        />
+        <Box
+          sx={{
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: "absolute",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography
+            onMouseEnter={handlePopoverOpen}
+            onMouseLeave={handlePopoverClose}
+            variant="caption"
+            component="div"
+            color="text.secondary"
+          >
+            {progress}
+          </Typography>
+          <Popover
+            id="mouse-over-popover"
+            onClick={handleClick}
+            sx={{
+              pointerEvents: "none",
+              top: "5%",
+              left: "38%",
+              // minWidth: 175,
+              width: "60%",
+            }}
+            open={open}
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            onClose={handlePopoverClose}
+            disableRestoreFocus
+          >
+            <Typography
+              sx={{ padding: 1, fontSize: 14, mb: 1, fontWeight: "bold" }}
+              color="text.primary"
+              gutterBottom
+            >
+              Model Printability Score is 75%
+            </Typography>
+
+            <Typography variant="body2" sx={{ padding: 1, minWidth: 75 }}>
+              25% of total printing time is wasted on printing support
+              materials. Try optimizing the model to prevent need for support
+              materials. .
+            </Typography>
+            <CardActions>
+              <Button size="small">
+                <Link to="/www.google.com">Learn More about 3D printing</Link>
+              </Button>
+            </CardActions>
+          </Popover>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default ProgressCircular;
---
+++ src/components/TableDetail.jsx
@@ -0,0 +1,41 @@
+import React, { Fragment } from 'react';
+import "./TableDetail.css";
+import ProgressCircular from './ProgressCircular';
+
+const TableDetail = ({ list }) => {
+
+
+
+    return (
+        <>
+            <table>
+                <tr>
+                    <th>Model Name</th>
+                    <th>Material</th>
+                    <th>Layer Height</th>
+                    <th>Sub Total</th>
+                    <th>Quantity</th>
+                    <th>Line Total</th>
+                    <th>Printability Score</th>
+                </tr>
+                {list.map((x, i) => (
+                    <Fragment key={i}>
+                        <tr>
+                            <td>{x.modelName}</td>
+                            <td>{x.material}</td>
+                            <td>{x.layerHeight}</td>
+                            <td>Rs. {x.subTotal}</td>
+                            <td>{x.quantity}</td>
+                            <td>Rs. {x.lineTotal}</td>
+                            <td><ProgressCircular progress={x.score} /></td>
+
+                        </tr>
+                    </Fragment>
+                ))}
+            </table>
+        </>
+    )
+
+}
+
+export default TableDetail;
